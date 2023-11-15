a080bdc3d4a518695fcecd20d7668cec76c81e78
audalio <audalio@cpqd.com.br>
2023-11-15 17:50:50 -0300

    2o Commit : consulta e retorno de marcas-modelos. Classe modelos

--- src/main/java/br/com/ajrdevops/TabelaFipe/principal/Principal.java
+++ src/main/java/br/com/ajrdevops/TabelaFipe/principal/Principal.java
@@ -1,13 +1,18 @@
 package br.com.ajrdevops.TabelaFipe.principal;
 
+import br.com.ajrdevops.TabelaFipe.model.Dados;
+import br.com.ajrdevops.TabelaFipe.model.Modelos;
 import br.com.ajrdevops.TabelaFipe.service.ConsumoApi;
+import br.com.ajrdevops.TabelaFipe.service.ConverteDados;
 
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Principal {
 
     private Scanner leitura = new Scanner(System.in);
     private ConsumoApi consumo = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
     private final String URL_BASE = "https://parallelum.com.br/fipe/api/v1/";
     public void exibeMenu() {
         var menu = """
@@ -16,7 +21,7 @@ public class Principal {
                 Moto
                 Caminhão
                 
-                Digite umas das opções para consultar: 
+                Digite umas das opções para consultar:
                 """;
 
         System.out.println(menu);
@@ -33,8 +38,25 @@ public class Principal {
         }
 
         var json = consumo.obterDados(endereco);
-
         System.out.println(json);
+
+        var marcas = conversor.obterLista(json, Dados.class);
+        marcas.stream()
+                .sorted(Comparator.comparing(Dados::nome))
+                .forEach(System.out::println);
+
+        System.out.println("Informe o código da marca para consulta:");
+        var codigoMarca = leitura.nextLine();
+
+        endereco = endereco + "/" + codigoMarca + "/modelos";
+        json = consumo.obterDados(endereco);
+        var modeloLista = conversor.obterDados(json, Modelos.class);
+
+        System.out.println("\n Modelos desta da Marca: ");
+        modeloLista.modelos().stream()
+                .sorted(Comparator.comparing(Dados::nome))
+                .forEach(System.out::println);
+
 
     }
 }
--- src/main/java/br/com/ajrdevops/TabelaFipe/service/IConverteDados.java
+++ src/main/java/br/com/ajrdevops/TabelaFipe/service/IConverteDados.java
@@ -1,7 +1,12 @@
 package br.com.ajrdevops.TabelaFipe.service;
 
 
+import java.util.List;
+
 public interface IConverteDados {
     <T> T obterDados(String json, Class<T> tClasse);
+
+    <T> List<T> obterLista(String json, Class<T> tClasse);
+
 }
 
